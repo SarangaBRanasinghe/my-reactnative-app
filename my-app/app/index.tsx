6a169f37d29d7f8d54272741e272561a7f5d9e25
Saranga Ranasinghe <ranasingherpksb.22@uom.lk>
2025-09-21 23:34:46 +0530

    re create

---
+++ my-app/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import "../global.css"
+const index = () => {
+  return (
+    <View>
+      <Text className='text-5xl font-bold text-green-700'>index</Text>
+    </View>
+  )
+}
+
+export default index
